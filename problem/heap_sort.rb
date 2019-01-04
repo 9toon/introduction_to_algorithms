9266c60d53d8300fdcd3ccafbb5cf47275672225
Yusuke Naito <yusuke-naito@haveagood.holiday>
2019-01-04 18:59:11 +0900

    Improve MyHeap#heap_sort

--- problem/heap_sort.rb
+++ problem/heap_sort.rb
@@ -1,64 +1,72 @@
-def heap_sort(arr)
-  (arr.length).downto(2) do |i|
-    tmp = arr[0]
-    arr[0] = arr[i - 1]
-    arr[i - 1] = tmp
-
-    arr.heap_size -= 1
-
-    max_heapify(arr, 1)
-  end
-
-  arr
-end
-
-def build_max_heap(arr)
-  arr.heap_size = arr.length
-
-  (arr.length / 2).downto(1) do |i|
-    max_heapify(arr, i)
-  end
-
-  arr
-end
-
-def max_heapify(arr, i)
-  l = left(i)
-  r = right(i)
-
-  if l <= arr.heap_size && arr[l - 1] > arr[i - 1]
-    largest = l
-  else
-    largest = i
-  end
-
-  if r <= arr.heap_size && arr[r - 1] > arr[largest - 1]
-    largest = r
-  end
-
-  if largest != i
-    tmp = arr[largest - 1]
-    arr[largest - 1] = arr[i - 1]
-    arr[i - 1] = tmp
-
-    max_heapify(arr, largest)
-  end
-
-  arr
-end
-
-def parent(i)
-  i / 2
-end
-
-def left(i)
-  2 * i
-end
-
-def right(i)
-  2 * i + 1
-end
-
 class MyHeap < Array
-  attr_accessor :heap_size
+  attr_reader :heap_size
+
+  def initialize(array)
+    super
+
+    build_max_heap
+  end
+
+  def heap_sort
+    (arr.length).downto(2) do |i|
+      tmp = arr[0]
+      arr[0] = arr[i - 1]
+      arr[i - 1] = tmp
+
+      @heap_size -= 1
+
+      max_heapify(1)
+    end
+
+    arr
+  end
+
+  private
+
+  def build_max_heap
+    @heap_size = arr.length
+
+    (arr.length / 2).downto(1) do |i|
+      max_heapify(i)
+    end
+  end
+
+  def max_heapify(i)
+    l = left(i)
+    r = right(i)
+
+    if l <= heap_size && arr[l - 1] > arr[i - 1]
+      largest = l
+    else
+      largest = i
+    end
+
+    if r <= heap_size && arr[r - 1] > arr[largest - 1]
+      largest = r
+    end
+
+    if largest != i
+      tmp = arr[largest - 1]
+      arr[largest - 1] = arr[i - 1]
+      arr[i - 1] = tmp
+
+      max_heapify(largest)
+    end
+  end
+
+  def parent(i)
+    i / 2
+  end
+
+  def left(i)
+    2 * i
+  end
+
+  def right(i)
+    2 * i + 1
+  end
+
+  def arr
+    self
+  end
 end
--- spec/heap_sort_spec.rb
+++ spec/heap_sort_spec.rb
@@ -1,36 +1,16 @@
 require_relative "../problem/heap_sort"
 
-describe "Problem heap_sort" do
+describe "MyHeap" do
   describe '#heap_sort' do
-    let(:arr) { build_max_heap(MyHeap.new([4, 1, 3, 2, 16, 9, 10, 14, 8, 7])) }
+    let(:items) { [4, 1, 3, 2, 16, 9, 10, 14, 8, 7] }
+    let(:heap) { MyHeap.new(items) }
 
-    it { expect(heap_sort(arr)).to eq([1, 2, 3, 4, 7, 8, 9, 10, 14, 16]) }
-  end
+    it { expect(heap.heap_sort).to eq(items.sort) }
 
-  describe '#build_max_heap' do
-    let(:arr) { MyHeap.new([4, 1, 3, 2, 16, 9, 10, 14, 8, 7]) }
+    context 'with huge num of items' do
+      let(:items) { (1..100_000).to_a.shuffle }
 
-    it { expect(build_max_heap(arr)).to eq([16, 14, 10, 8, 7, 9, 3, 2, 4, 1]) }
-  end
-
-  describe '#max_heapify' do
-    let(:arr) { MyHeap.new([16, 4, 10, 14, 7, 9, 3, 2, 8, 1]) }
-    let(:i) { 2 }
-
-    before { arr.heap_size = arr.size }
-
-    it { expect(max_heapify(arr, i)).to eq([16, 14, 10, 8, 7, 9, 3, 2, 4, 1]) }
-  end
-
-  describe '#parent' do
-    it { expect(parent(2)).to eq(1) }
-  end
-
-  describe '#left' do
-    it { expect(left(2)).to eq(4) }
-  end
-
-  describe '#right' do
-    it { expect(right(2)).to eq(5) }
+      it { expect(heap.heap_sort).to eq(items.sort) }
+    end
   end
 end
